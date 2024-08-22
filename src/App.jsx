1fe1c1e2738b6b3a0b96f0e35bc8eca03a94f63e
NaveenPattihal <naveenpattihal08@gmail.com>
2024-08-22 21:04:39 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return(
     <>
+    <Navbar />
     <p >Coin Table</p>
     </>
   )
