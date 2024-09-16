257771e24b6bb5784d7cac376f35c651408186d2
NaveenPattihal <naveenpattihal08@gmail.com>
2024-09-16 21:53:41 +0530

    started consuming FetchCoinDetails

--- src/Pages/CoinDetails.jsx
+++ src/Pages/CoinDetails.jsx
@@ -1,6 +1,76 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { FetchCoinDetails } from "../services/fecthCoinDetails";
+import parse from 'html-react-parser';
+import CurrencyStore from "../Store/CurrencyStore";
+
 function CoinDetails() {
+
+    const { coinId } = useParams()
+    const { currency } = CurrencyStore()
+
+    const {data: coin , isLoading , isError} = useQuery({
+        queryKey: ['coin', coinId],
+        queryFn: () => FetchCoinDetails(coinId),
+        cacheTime: 1000 * 60 * 2,
+        staleTime: 1000 * 60 * 2
+    })
+
+    if(isLoading){
+        return <div>Loading Coin Data ...</div>
+    }
+
+    if(isError){
+        console.log(Error);
+    }
+
+    if(coin){
+        console.log(coin);
+        
+    }
+
     return(
-        <div>Coin Deatils page</div>
+        <div className="flex flex-col md:flex-row">
+            <div className="flex flex-col w-full md:w-1/3 border-slate-400 border-r-2">
+                <div className="flex flex-row gap-5 items-center justify-center my-5">
+                    <div>
+                        <img 
+                        src={coin?.image?.large} 
+                        alt={coin.id} 
+                        />
+                    </div>
+                    <div>
+                        <h1 
+                        className="text-5xl font-bold">
+                            {coin.name}
+                        </h1>
+                        <p 
+                        className="text-2xl">
+                            {coin.symbol}
+                        </p>
+                    </div>
+                </div>
+
+                <div>
+                    <p 
+                    className="py-2 px-5 text-justify">
+                        {parse(coin?.description?.en)}
+                    </p>
+                </div>
+
+                <div className="flex flex-col md:flex-row justify-around py-2 px-5">
+                    <div className="flex flex-row gap-2">
+                        <h1 className="text-2xl font-bold text-yellow-500">Rank:</h1>
+                        <p className="text-2xl font-bold">{coin.market_cap_rank}</p>
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <h1 className="text-2xl font-bold text-green-500">Current Price:</h1>
+                        <p className="text-2xl font-bold">{coin?.market_data?.current_price?.[currency]}</p>
+                    </div>
+                </div>
+            </div>
+            <div>Price-Plot Chart</div>
+        </div>
     )
 }
 
