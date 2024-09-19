c69db70ade99c5273fcfdae6d09da82380dd3254
NaveenPattihal <naveenpattihal08@gmail.com>
2024-09-19 23:27:10 +0530

    added infiniteScroll for CoinTable

--- src/Components/CoinTable/CoinTable.jsx
+++ src/Components/CoinTable/CoinTable.jsx
@@ -1,13 +1,14 @@
-import { useEffect, useState} from "react";
+
 import { FetchCoinData } from "../../services/FetchCoinData";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import CurrencyStore from "../../Store/CurrencyStore";
 import { useNavigate } from "react-router-dom";
 import MyBulletListLoader from "../Loaders/BulletListLoader";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function CoinTable(){
 
-    const [page , setPage] = useState(1);
+    
     const {currency} = CurrencyStore();
     const navigate = useNavigate();
 
@@ -15,16 +16,20 @@ function CoinTable(){
         navigate(`/details/${id}`)
     }
 
-    const { data, isLoading , error, isError} = useQuery({
-        queryKey: ['coins', page, currency], // Use 'queryKey' instead of positional argument
-        queryFn: () => FetchCoinData(page, currency), // Use 'queryFn' instead of positional argument
-        cacheTime: 1000 * 2 * 60, // Still part of the options
-        staleTime: 1000 * 2 * 60, // Still part of the options
-    });
+    const{data, 
+        fetchNextPage, 
+        hasNextPage, 
+        isFetchingNextPage,
+        isLoading, 
+        isError, 
+        error, } = useInfiniteQuery({
+        queryKey : ['coins' , currency],
+        queryFn : ({ pageParam = 1 }) => FetchCoinData(pageParam, currency),
+        getNextPageParam : (lastPage , allPages) => lastPage.length ? allPages.length + 1 : undefined,
+        cacheTime : 1000 * 60 * 2,
+        staleTime : 1000 * 60 * 2,
+    })
 
-    // if (isLoading) {
-    //     return <div>Loading....</div>
-    // }
 
     if (isError) {
         console.log(error);
@@ -32,12 +37,14 @@ function CoinTable(){
     }
 
     if(data){
-        console.log(data);
+        const page = data.pages[0];
+        const coin = page[0];
+        console.log(coin.id);
         
     }
 
     return(
-        <div>
+        
             
             <div className="flex flex-col items-center justify-center w-[80vw] my-5 mx-auto">
                 <div className="flex items-center justify-center bg-yellow-400 w-full gap-3 py-5 px-5 font-semibold text-2xl text-black text-center ">
@@ -55,11 +62,22 @@ function CoinTable(){
                     </div>
                 </div>
 
+                <InfiniteScroll
+                    dataLength={data ? data.pages.flat().length : 0} 
+                    next={fetchNextPage}
+                    hasMore={hasNextPage}
+                    loader={<MyBulletListLoader/>}
+                    endMessage={
+                    <p style={{ textAlign: 'center' }}>
+                        <b>Yay! You have seen it all</b>
+                    </p>
+                    }
+                >
                 <div className="flex flex-col items-center justify-center w-[80vw] my-5 mx-auto">
-                    {isLoading && <MyBulletListLoader/>}
-                    {data && data.map(coin => {
+                    {data && data?.pages.flat().map((coin) => {
                         return(
-                            <div key={coin.id} onClick={() => handleCoinDetailsRedirect(coin.id)} className="flex items-center justify-center w-full gap-3 py-5 px-5 cursor-pointer">
+                            
+                                <div key={coin.id} onClick={() => handleCoinDetailsRedirect(coin.id)} className="flex items-center justify-center w-full gap-3 py-5 px-5 cursor-pointer">
                                 <div className="flex gap-5 basis-[35%]">
                                     <div className="w-[5rem] h-[5rem]">
                                         <img src={coin.image} alt="" className="w-full h-full"/>
@@ -80,28 +98,13 @@ function CoinTable(){
                                     {coin.high_24h}
                                 </div>
                             </div>
+                            
                         )
                     })}
                 </div>
+                </InfiniteScroll>
+                {isFetchingNextPage && <MyBulletListLoader />}
             </div>
-
-            <div className="flex items-center justify-center gap-5  w-[80vw] my-5 mx-auto">
-                <button
-                disabled = {page === 1}
-                onClick={() => {
-                    setPage(page - 1)
-                }} 
-                className="py-5 px-20 bg-blue-700 text-white">
-                    Prev
-                </button>
-                <button onClick={() => {
-                    setPage(page + 1)
-                }} 
-                className="py-5 px-20 bg-green-700 text-white">
-                    Next
-                </button>
-            </div>
-        </div>
 
     )
 }
