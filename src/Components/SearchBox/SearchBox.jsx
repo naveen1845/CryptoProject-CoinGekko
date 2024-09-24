e0fadd84aa95619ea8a71fd7d2fee91c21aadb9c
NaveenPattihal <naveenpattihal08@gmail.com>
2024-09-24 15:39:39 +0530

    fix: fixed onClick Bug in Search

--- src/Components/SearchBox/SearchBox.jsx
+++ src/Components/SearchBox/SearchBox.jsx
@@ -3,11 +3,13 @@ import SearchItem from "./SearchItem";
 
 function SearchBox() {
     const {data ,isError, error , isLoading, searchText, setSearchText, debounceValue} = useFetchCoinSearchData()
+    
 
     return (
         <div className="parent">
             <div className="dropdown dropdown-end">
                 <input 
+                    tabIndex={0}
                     type="text"
                     value={searchText}
                     onChange={(event) => setSearchText(event.target.value)}
@@ -15,13 +17,14 @@ function SearchBox() {
                     className="input input-bordered w-24 md:w-auto"
                 />
 
-                {debounceValue && <ul 
+                {debounceValue && <ul
+                    tabIndex={0} 
                     className="dropdown-content flex flex-col justify-center items-center bg-base-200 mt-3 rounded-box z-[1] w-72 md:w-96 left-[-160px] p-2 shadow"
                 >
                     {isLoading && <span className="loading loading-dots loading-lg"></span>}
                     {(data && data.length == 0) && <li>No Coin Found</li>}
                     {isError && <span>Error Fetching Data</span>}
-                    {data && data.map((item) => <div className="w-full" key={item.id}><SearchItem coinData={item}/></div>)}
+                    {data && data?.map(item => <div className='mb-2 w-full' key={item.id}><SearchItem coinData={item}/></div>)}
                 </ul>}
             </div>
         </div>
--- src/Components/SearchBox/SearchItem.jsx
+++ src/Components/SearchBox/SearchItem.jsx
@@ -3,20 +3,24 @@ import React from 'react'
 
 function SearchItem({coinData}) {
     
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
-    function handleCoinDetailsRedirect(id){
-        navigate(`/details/${id}`)
+    console.log(coinData.id);
+    
+
+    function handleCoinDetailsRedirect(id) {
+        console.log('Navigating to:', `/details/${id}`);
+        navigate(`/details/${id}`);
     }
 
     return (
-        <li className="w-full hover:bg-black rounded-lg">
-            <a href={`/details/${coinData.id}`} className="flex justify-between items-center h-12 cursor-pointer p-5 ">
-                <img className="h-8" src={coinData.thumb}  alt={coinData.name}/>
-                <p className="font-semibold">{coinData.name} - <span className="font-thin">{coinData.symbol}</span></p>
-                <p>Rank <span className="text-yellow-500">{coinData.market_cap_rank}</span></p>
-            </a>
-        </li>
+        <li className=' w-full hover:bg-black cursor-pointer rounded-lg' onClick={() => handleCoinDetailsRedirect(coinData.id)}>
+        <a className='flex justify-between items-center h-12 cursor-pointer p-5'>
+            <img src={coinData.thumb} className='h-8' alt={coinData.name} />
+            <p className='font-semibold'>{coinData.name} - <span className='font-thin'>{coinData.symbol}</span></p>
+            <p className='text-yellow-500'>👑{coinData.market_cap_rank}</p>
+        </a>
+    </li>
     )
 }
 
